ce80b1426a9b87cab87428fae8b7cf27fa28fbfc
Sergej Radkovec <radkovecsergej@gmail.com>
2018-12-19 18:56:45 +0300

    add style for validation form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -41,11 +41,11 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
                 },
                 valid: false,
-                minLength: 5,
-                maxLength: 5,
             },
             country: {
                 elementType: 'input',
@@ -153,6 +153,8 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
                 <Button btnType="Success" >ORDER</Button>
