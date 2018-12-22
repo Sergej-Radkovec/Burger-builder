6ff11e9dadd73418535caf50761ebe736dd5a6ff
Sergej Radkovec <radkovecsergej@gmail.com>
2018-12-22 17:41:20 +0300

    connect contactData & actions

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -35,8 +35,13 @@ const reducer = (state = initialState, action) => {
             };
         case actionTypes.SET_INGREDIENTS:
             return {
-                ...state,
-                ingredients: action.ingredients,
+                ...state, 
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    bacon: action.ingredients.bacon,
+                    cheese: action.ingredients.cheese,
+                    meat: action.ingredients.meat
+                },
                 error: false
             };
         case actionTypes.FETCH_INGREDIENTS_FAILED:
